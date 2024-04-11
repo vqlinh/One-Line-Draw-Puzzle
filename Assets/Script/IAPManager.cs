0f1de9a42aecebde7ca691f298b409ca342fa87d
DESKTOP-TLMPEQQ\Admin <haodang@smartwave247.com>
2024-04-11 15:14:41 +0700

    no message

--- Assets/Script/IAPManager.cs
+++ Assets/Script/IAPManager.cs
@@ -16,16 +16,16 @@ public class ConsumableItem {
 [System.Serializable]
 public class IAPManager : MonoBehaviour, IStoreListener
 {
- //   public ShopController shopController;
+    public ShopController shopController;
     public ConsumableItem[] listItems;
-    const string COIN_5 = "com.onelinedrawpuzzle.pack1";
-    const string COIN_11 = "com.onelinedrawpuzzle.pack2";
-    const string COIN_17 = "com.onelinedrawpuzzle.pack3";
-    const string COIN_23 = "com.onelinedrawpuzzle.pack4";
-    const string COIN_29 = "com.onelinedrawpuzzle.pack5";
-    const string COIN_35 = "com.onelinedrawpuzzle.pack6";
-    const string COIN_41 = "com.onelinedrawpuzzle.pack7";
-    const string COIN_47 = "com.onelinedrawpuzzle.pack8";
+    const string PACK_1 = "com.onelinedrawpuzzle.pack1";
+    const string PACK_2 = "com.onelinedrawpuzzle.pack2";
+    const string PACK_3 = "com.onelinedrawpuzzle.pack3";
+    const string PACK_4 = "com.onelinedrawpuzzle.pack4";
+    const string PACK_5 = "com.onelinedrawpuzzle.pack5";
+    const string PACK_6 = "com.onelinedrawpuzzle.pack6";
+    const string PACK_7 = "com.onelinedrawpuzzle.pack7";
+    const string PACK_8 = "com.onelinedrawpuzzle.pack8";
 
     IStoreController m_StoreController;
     // void Start()
@@ -50,35 +50,35 @@ public class IAPManager : MonoBehaviour, IStoreListener
     /** UI BUTTON EVENTs for PURCHASE **/
     public void Consumable_BtnCoin5_Pressed()
     {
-        m_StoreController.InitiatePurchase(COIN_5);
+        m_StoreController.InitiatePurchase(PACK_1);
     }
     public void Consumable_BtnCoin11_Pressed()
     {
-        m_StoreController.InitiatePurchase(COIN_11);
+        m_StoreController.InitiatePurchase(PACK_2);
     }
     public void Consumable_BtnCoin17_Pressed()
     {
-        m_StoreController.InitiatePurchase(COIN_17);
+        m_StoreController.InitiatePurchase(PACK_3);
     }
     public void Consumable_BtnCoin23_Pressed()
     {
-        m_StoreController.InitiatePurchase(COIN_23);
+        m_StoreController.InitiatePurchase(PACK_4);
     }
     public void Consumable_BtnCoin29_Pressed()
     {
-        m_StoreController.InitiatePurchase(COIN_29);
+        m_StoreController.InitiatePurchase(PACK_5);
     }
     public void Consumable_BtnCoin35_Pressed()
     {
-        m_StoreController.InitiatePurchase(COIN_35);
+        m_StoreController.InitiatePurchase(PACK_6);
     }
     public void Consumable_BtnCoin41_Pressed()
     {
-        m_StoreController.InitiatePurchase(COIN_41);
+        m_StoreController.InitiatePurchase(PACK_7);
     }
     public void Consumable_BtnCoin47_Pressed()
     {
-        m_StoreController.InitiatePurchase(COIN_47);
+        m_StoreController.InitiatePurchase(PACK_8);
     }
     public void OnInitializeFailed(InitializationFailureReason error)
     {
@@ -90,46 +90,46 @@ public class IAPManager : MonoBehaviour, IStoreListener
         //Restrive the purchased product
         var product = purchaseEvent.purchasedProduct;
         Debug.Log("Purchase completed: " + product.definition.id);
-        if (product.definition.id == COIN_5)
+        if (product.definition.id == PACK_1)
         {
             //Add Coins
             Debug.Log("Plus 5 coins");
-         //   shopController.AddCoin(5);
+            shopController.AddCoin(5);
         }
-        else if (product.definition.id == COIN_11)
+        else if (product.definition.id == PACK_2)
         {
             Debug.Log("Plus 11 coins");
-      //      shopController.AddCoin(11);
+            shopController.AddCoin(11);
         }
-        else if (product.definition.id == COIN_17)
+        else if (product.definition.id == PACK_3)
         {
             Debug.Log("Plus 17 coins");
-       //     shopController.AddCoin(17);
+            shopController.AddCoin(17);
         }
-        else if (product.definition.id == COIN_23)
+        else if (product.definition.id == PACK_4)
         {
             Debug.Log("Plus 23 coins");
-        //    shopController.AddCoin(23);
+            shopController.AddCoin(23);
         }
-        else if (product.definition.id == COIN_29)
+        else if (product.definition.id == PACK_5)
         {
             Debug.Log("Plus 29 coins");
-     //       shopController.AddCoin(29);
+            shopController.AddCoin(29);
         }
-        else if (product.definition.id == COIN_35)
+        else if (product.definition.id == PACK_6)
         {
             Debug.Log("Plus 35 coins");
-     //      shopController.AddCoin(35);
+            shopController.AddCoin(35);
         }
-        else if (product.definition.id == COIN_41)
+        else if (product.definition.id == PACK_7)
         {
             Debug.Log("Plus 41 coins");
-     //       shopController.AddCoin(41);
+            shopController.AddCoin(41);
         }
-        else if (product.definition.id == COIN_47)
+        else if (product.definition.id == PACK_8)
         {
             Debug.Log("Plus 47 coins");
-      //      shopController.AddCoin(47);
+            shopController.AddCoin(47);
         }
         return PurchaseProcessingResult.Complete;
     }
